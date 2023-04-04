12e5296f552c80fedf0beb041b2a831e5e983a43
Victor <vt.victortran@gmail.com>
2023-04-03 18:43:41 -0700

    navbar fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,9 +31,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="w-full bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <a href="https://www.holdupclimbing.com/" className="flex items-center">
+          <img src="./logo.png" className="h-8 mr-3" alt="Holdup Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Holdup</span>
         </a>
         <button
           onClick={handleMenuToggle}
@@ -48,13 +48,13 @@ const Navbar: React.FC = () => {
           </svg>
         </button>
         <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-          <nav className="hidden md:flex space-x-10">
-              <NavItem href="/about" text="About"/>
-              <NavItem href="/portfolio" text="Portfolio"/>
-              <NavItem href="/gallery" text="Gallery"/>
+        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              {/* <a href="#" className="py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a> */}
+              <a href="#" className='text-black block px-3 py-2 text-base font-medium md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all' aria-current="page">About</a>
+              <a href="#" className='text-black block px-3 py-2 text-base font-medium md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all' aria-current="page">Roadmap</a>
+              <a href="#" className='text-black block px-3 py-2 text-base font-medium md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all' aria-current="page">Contact</a>
               <button id='resume' className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-2xl shadow-sm text-base font-medium text-white bg-indigo-500 hover:bg-indigo-700">Login</button>
-          </nav>
-          
+          </ul>
         </div>
       </div>
     </nav>
